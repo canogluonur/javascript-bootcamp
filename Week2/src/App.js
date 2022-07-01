d9bd1de10978914e6bbdeb93509e3560bf2a3a74
Onur <58385909+canogluonur@users.noreply.github.com>
2022-07-01 13:54:50 +0300

    Add files via upload

---
+++ Week2/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+import Lessons from "./components/Lessons/Lessons";
+
+import "./App.css";
+
+const App = () => {
+
+  return (
+    <section className="app">
+      <Header></Header>
+      <Lessons></Lessons>
+    </section>
+  );
+}
+
+export default App;
